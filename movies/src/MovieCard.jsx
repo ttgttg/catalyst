4fb20415d574a5e9780b591a13dc03de04656599
CY <pchinyun@gmail.com>
2024-08-18 15:47:52 +1000

    sort preferances into arrays and css styling

--- movies/src/MovieCard.jsx
+++ movies/src/MovieCard.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+ import React from "react";
 
-const MovieCard = ({movie1}) => { 
-    return ( 
+const MovieCard = ({ movie1, onNope, onLove }) => {
+    return (
         <>
-<div className='movie'>
-            <p>{movie1.Year}</p>
-        </div>
+            <div className='movie'>
+                <p>{movie1.Year}</p>
+            </div>
 
-        <div>
-            <img src={ movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/> 
-        </div>
-        
-        <div> 
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-        </div>
+            <div>
+                <img 
+                    src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/200'} 
+                    alt={movie1.Title}
+                />
+            </div>
+
+            <div>
+                <span>{movie1.Type}</span>
+                <h3>{movie1.Title}</h3>
+            </div>
+
+            <button onClick={() => onNope(movie1)}>Nope</button>
+            <button onClick={() => onLove(movie1)}>Love this</button>
         </>
-    )
+    );
 }
 
-export default MovieCard; 
+export default MovieCard;
